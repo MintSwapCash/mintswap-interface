6d1046df83675a6c561b13abd18307d945563782
Jones <86437208+prsstech@users.noreply.github.com>
2022-02-18 19:03:31 +0100

    Update archerRouter.ts

--- src/functions/archerRouter.ts
+++ src/functions/archerRouter.ts
@@ -2,13 +2,13 @@ import {
   ChainId,
   Currency,
   CurrencyAmount,
-  SmartBCH,
+  MINTME,
   SwapParameters,
   Trade,
   TradeOptions,
   TradeOptionsDeadline,
   TradeType,
-} from '@mistswapdex/sdk'
+} from '@mintswapcash/sdk'
 
 import { getAddress } from '@ethersproject/address'
 import invariant from 'tiny-invariant'
@@ -69,12 +69,12 @@ export abstract class ArcherRouter {
     trade: Trade<Currency, Currency, TradeType>,
     options: ArcherTradeOptions
   ): ArcherSwapParameters {
-    const etherIn = trade.inputAmount.currency === SmartBCH.onChain(ChainId.SMARTBCH)
-    const etherOut = trade.outputAmount.currency === SmartBCH.onChain(ChainId.SMARTBCH)
+    const etherIn = trade.inputAmount.currency === MINTME.onChain(ChainId.MINTME)
+    const etherOut = trade.outputAmount.currency === MINTME.onChain(ChainId.MINTME)
     // the router does not support both ether in and out
     invariant(!(etherIn && etherOut), 'ETHER_IN_OUT')
     invariant(!('ttl' in options) || options.ttl > 0, 'TTL')
-    invariant('ethTip' in options && options.ethTip?.currency === SmartBCH.onChain(ChainId.SMARTBCH))
+    invariant('ethTip' in options && options.ethTip?.currency === MINTME.onChain(ChainId.MINTME))
 
     const to: string = validateAndParseAddress(options.recipient)
     const amountInCurrency = trade.maximumAmountIn(options.allowedSlippage)
