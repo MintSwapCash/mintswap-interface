855b17ace9ceba85a17eb0040e8e5eec9cd5a13c
Jones <86437208+prsstech@users.noreply.github.com>
2022-02-18 19:58:15 +0100

    Update settings.tsx

--- src/pages/user/settings.tsx
+++ src/pages/user/settings.tsx
@@ -4,8 +4,8 @@ export default function Settings() {
   return (
     <Container id="settings-page" className="py-4 space-y-3 md:py-8 lg:py-12" maxWidth="2xl">
       <Head>
-        <title>Settings | Mist</title>
-        <meta name="description" content="MISTswap Settings..." />
+        <title>Settings | Mint</title>
+        <meta name="description" content="MintSwap Settings..." />
       </Head>
     </Container>
   )
