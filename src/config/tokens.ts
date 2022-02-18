329c79f104772179d629edb2274b9e448e58b267
Jones <86437208+prsstech@users.noreply.github.com>
2022-02-18 18:18:34 +0100

    Update tokens.ts

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -1,24 +1,21 @@
-import { ChainId, MIST_ADDRESS, BAR_ADDRESS, Token, WBCH} from '@mistswapdex/sdk'
+import { ChainId, MINT_ADDRESS, BAR_ADDRESS, Token, WMINT} from '@mintswapcash/sdk'
 
-export const FLEXUSD = new Token(ChainId.SMARTBCH, '0x7b2B3C5308ab5b2a1d9a94d20D35CCDf61e05b72', 18, 'flexUSD', 'flexUSD')
-export const LAWUSD = new Token(ChainId.SMARTBCH, '0xE1E655BE6F50344e6dd708c27BD8D66492d6ecAf', 18, 'lawUSD', 'LAW US Dollar')
+export const FLEXUSD = new Token(ChainId.SMARTBCH, '0xe249f3537af85cef70dfe35cfa12062f7a43d95a', 18, 'flexUSD', 'flexUSD')
 
-export const XMIST: ChainTokenMap = {
-    [ChainId.SMARTBCH]: new Token(ChainId.SMARTBCH, BAR_ADDRESS[ChainId.SMARTBCH], 18, 'xMIST', 'MistBar'),
-    [ChainId.SMARTBCH_AMBER]: new Token(ChainId.SMARTBCH_AMBER, BAR_ADDRESS[ChainId.SMARTBCH_AMBER], 18, 'xMIST', 'MistBar'),
+export const XMINT: ChainTokenMap = {
+    [ChainId.SMARTBCH]: new Token(ChainId.SMARTBCH, BAR_ADDRESS[ChainId.SMARTBCH], 18, 'xMINT', 'MintSwap Bar'),
 }
 
 type ChainTokenMap = {
   readonly [chainId in ChainId]?: Token
 }
 
-export const MIST: ChainTokenMap = {
-  [ChainId.SMARTBCH]: new Token(ChainId.SMARTBCH, MIST_ADDRESS[ChainId.SMARTBCH], 18, 'MIST', 'MistToken'),
-  [ChainId.SMARTBCH_AMBER]: new Token(ChainId.SMARTBCH_AMBER, MIST_ADDRESS[ChainId.SMARTBCH_AMBER], 18, 'MIST', 'MistToken'),
+export const MINT: ChainTokenMap = {
+  [ChainId.MINTME]: new Token(ChainId.MINTME, MINT_ADDRESS[ChainId.MINTME], 18, 'MINT', 'MintSwap Token'),
 }
 
-export const WBCH_EXTENDED: { [chainId: number]: Token } = {
-  ...WBCH,
+export const WMINT_EXTENDED: { [chainId: number]: Token } = {
+  ...WMINT,
 }
 
 type ChainTokenMapList = {
@@ -26,10 +23,7 @@ type ChainTokenMapList = {
 }
 
 // These are available for migrate
-export const BENSWAP_TOKENS: ChainTokenMapList = {
+export const MINTSWAP_TOKENS: ChainTokenMapList = {
   [ChainId.SMARTBCH]: [
-  ],
-  [ChainId.SMARTBCH_AMBER]: [
-    new Token(ChainId.SMARTBCH_AMBER, '0x842692f8A4D0743e942dF5D52155a037327d4f3f', 18, 'EBENS/BCH LP Token', 'EBEN-BCH'),
   ],
 }
