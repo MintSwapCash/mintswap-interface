15f81d8be0b8ded96c2d34825d0e3da0f9b79dcf
Jones <86437208+prsstech@users.noreply.github.com>
2022-02-19 16:50:40 +0100

    Update hooks.ts

--- src/state/claim/hooks.ts
+++ src/state/claim/hooks.ts
@@ -127,7 +127,7 @@ export function useClaimCallback(account: string | null | undefined): {
         })
         .then((response: TransactionResponse) => {
           addTransaction(response, {
-            summary: `Claimed ${unClaimedAmount?.toSignificant(4)} MIST`,
+            summary: `Claimed ${unClaimedAmount?.toSignificant(4)} MINT`,
             claim: { recipient: account },
           })
           return response.hash
